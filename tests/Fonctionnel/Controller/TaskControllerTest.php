c338406cf18c1609b02f2d138601cb9123b22575
jc <k4leth@gmail.com>
2024-06-23 18:42:19 +0200

    update test controller and entity for codacy

--- tests/Fonctionnel/Controller/TaskControllerTest.php
+++ tests/Fonctionnel/Controller/TaskControllerTest.php
@@ -17,8 +17,9 @@ class TaskControllerTest extends WebTestCase
     /**
      * The setUp function initializes the client and entity manager for testing purposes in a PHP
      * environment.
+     * 
+     * @return void
      */
-
     public function setUp(): void
     {
         $this->client = static::createClient();
@@ -33,11 +34,13 @@ class TaskControllerTest extends WebTestCase
      * @param user The `` parameter in the `login` function likely represents the username or
      * email address of the user trying to log in. It is used to populate the `_username` field in the
      * login form when submitting the form for authentication.
+     * 
      * @param password The `login` function you provided seems to be a part of a PHP application that
      * handles user authentication. The function is responsible for logging in a user by submitting a
      * form with the user's credentials (username and password).
+     * 
+     * @return void
      */
-
     public function login($user, $password): void
     {
         $crawler = $this->client->request('GET', '/login');
@@ -48,8 +51,9 @@ class TaskControllerTest extends WebTestCase
     /**
      * The testListTask function logs in as an admin, sends a GET request to '/task', and asserts that
      * the response status code is 200.
+     * 
+     * @return void
      */
-
     public function testListTask(): void
     {
         $this->login('Admin', 'admin');
@@ -61,8 +65,9 @@ class TaskControllerTest extends WebTestCase
     /**
      * The testCreateAction function logs in as an admin, creates a new task with a title and content,
      * and asserts that the task was successfully added.
+     * 
+     * @return void
      */
-
     public function testCreateAction(): void
     {
         $this->login('Admin', 'admin');
@@ -77,8 +82,9 @@ class TaskControllerTest extends WebTestCase
      * This function tests the editing functionality of a task by logging in as an admin, submitting a
      * form with modified task title and content, and asserting the success message displayed after the
      * task is edited.
+     * 
+     * @return void
      */
-
     public function testEditAction(): void
     {
         $this->login('Admin', 'admin');
@@ -92,8 +98,9 @@ class TaskControllerTest extends WebTestCase
     /**
      * The function `testToggleTaskAction` tests toggling a task's completion status in a PHP
      * application.
+     * 
+     * @return void
      */
-
     public function testToggleTaskAction(): void
     {
         $this->login('Admin', 'admin');
@@ -108,8 +115,9 @@ class TaskControllerTest extends WebTestCase
      * The testDeleteTaskAction function tests the deletion of a task by logging in as an admin,
      * selecting the delete button, sending a request to delete a specific task, and asserting the
      * success message after deletion.
+     * 
+     * @return void
      */
-
     public function testDeleteTaskAction(): void
     {
 
--- tests/Unitaire/Entity/UserTest.php
+++ tests/Unitaire/Entity/UserTest.php
@@ -14,11 +14,14 @@ class UserTest extends KernelTestCase
      * 
      * @param string email The email parameter in the `getEntity` function is a string that represents
      * the email address of the user.
+     * 
      * @param string username The username parameter in the `getEntity` function is a string that
      * represents the username of the user you are creating.
-     * @param string password The `getEntity` function takes four parameters: ``, ``,
-     * ``, and ``. The function creates a new `User` object, sets the email, username,
+     * 
+     * @param string password The `getEntity` function takes four parameters: `email`, `username`,
+     * `password`, and `role`. The function creates a new `User` object, sets the email, username,
      * password, and roles for the user, and then returns the user object.
+     * 
      * @param array role The `role` parameter in the `getEntity` function is an array that contains the
      * roles assigned to the user. These roles determine the permissions and access levels that the
      * user has within the system. For example, roles could be 'admin', 'editor', 'user', etc.
@@ -26,7 +29,6 @@ class UserTest extends KernelTestCase
      * @return User An instance of the User class with the provided email, username, password, and
      * roles set.
      */
-
     public function getEntity(string $email, string $username, string $password, array $role): User
     {
         $user = new User();
@@ -41,13 +43,15 @@ class UserTest extends KernelTestCase
     /**
      * This PHP function counts the number of validation errors for a given User object.
      * 
-     * @param User user The `countError` function takes two parameters:
-     * @param int number The `` parameter in the `countError` function is an optional integer
+     * @param User user The `countError` function takes two parameters `user` and `number`:
+     * 
+     * @param int number The `number` parameter in the `countError` function is an optional integer
      * parameter that specifies the expected count of errors that should be found when validating the
      * `User` object. If no value is provided for this parameter, the default value is set to 0. This
      * parameter allows you to specify
+     * 
+     * @return void
      */
-
     public function countError(User $user, int $number = 0): void
     {
         self::bootKernel();
@@ -62,8 +66,9 @@ class UserTest extends KernelTestCase
     /**
      * The function `testGood` creates a user entity with specific details and checks if there are any
      * errors associated with it.
+     * 
+     * @return void
      */
-
     public function testGood(): void
     {
         $user = $this->getEntity('test@email.com', 'superUser', '123456', ['ROLE_ADMIN']);
@@ -72,8 +77,9 @@ class UserTest extends KernelTestCase
 
     /**
      * The testError function checks for errors in creating entities with invalid data.
+     * 
+     * @return void
      */
-    
     public function testError(): void
     {
         self::countError($this->getEntity('test@', 'superUser', '123456', ['ROLE_ADMIN']), 1);
